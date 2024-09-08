c247cf4de3ce47351a7576b32e0c4877a9cdb064
eonsyn <154956467+eonsyn@users.noreply.github.com>
2024-09-09 00:01:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 const socketIO = require("socket.io");
 
 const app = express();
